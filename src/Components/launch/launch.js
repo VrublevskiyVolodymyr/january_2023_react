a57897d17923553b01ae89f955ec1bbbfc35c8a8
VrublevskiyVolodymyr <115405839+VrublevskiyVolodymyr@users.noreply.github.com>
2023-01-19 19:22:25 +0200

    Add Lesson_2_Launches_change

--- src/Components/launch/launch.js
+++ src/Components/launch/launch.js
@@ -1,8 +1,8 @@
-const Launch = ({launch }) => {
+const Launch = ({launch}) => {
 
     const {mission_name, launch_year, mission_patch_small} = launch
     return (
-        <div>
+        <div className={launch}>
             <div>mission_name: {mission_name}</div>
             <div>launch_year: {launch_year}</div>
             <div>mission_patch_small: {mission_patch_small}</div>
