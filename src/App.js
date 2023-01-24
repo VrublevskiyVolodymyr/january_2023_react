6dffb89c55dc25ab676c248c131a2a63a562200b
VrublevskiyVolodymyr <115405839+VrublevskiyVolodymyr@users.noreply.github.com>
2023-01-24 19:59:11 +0200

    Add Homework_3_Cars

--- src/App.js
+++ src/App.js
@@ -11,22 +11,15 @@ const App = () => {
     return (
         <div>
 
-            {/*Зробити компонент, в якому буде форма, за допомоги якої можливо створити нового юзера постовим запитом на http://jsonplaceholder.typicode.com/users*/}
+
             <UserForm/>
 
             <hr/>
 
             <Users setUserId={setUserId}/>
 
-            <hr/>
 
-            {/*Зробити компонент, в якому буде форма, за допомоги якої можливо створити новий комментар постовим запитом на http://jsonplaceholder.typicode.com/comments*/}
 
-            <PostForm/>
-
-            <hr/>
-
-            <UserPosts userId={userId}/>
 
         </div>
     );
