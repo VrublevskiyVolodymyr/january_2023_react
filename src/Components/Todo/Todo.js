c12b8de179ed92974153e3c5134b676bb42e7de8
VrublevskiyVolodymyr <115405839+VrublevskiyVolodymyr@users.noreply.github.com>
2023-01-27 14:10:43 +0200

    Update

--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -4,12 +4,13 @@ import css from './Todo.module.css'
 
 const Todo = ({todo}) => {
     const {id, userId, title, completed} = todo
+    console.log(todo)
     return (
         <div className={css.Todo}>
             <div>id: {id} </div>
             <div>userId: {userId} </div>
             <div>title: {title} </div>
-            <div>completed: {completed} </div>
+            <div>completed: {completed.toString()} </div>
         </div>
     );
 };
